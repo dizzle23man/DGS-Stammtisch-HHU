49fe2bd9e624dbe82c7702f03ecb940655f0fc65
dizzle23man <richard.pal22@gmail.com>
2026-05-03 00:43:30 +0200

    v1.2.8
    changed the widged to Behold (free basic)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DGS Stammtisch Hamburg – Service Worker
    =================================================== */
 
-const CACHE_NAME = 'dgs-hh-v8';
+const CACHE_NAME = 'dgs-hh-v9';
 
 const PRECACHE = [
   './',
