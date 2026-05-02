4e6ea7b80e3a26776008c7335e8ee48b79c16940
dizzle23man <richard.pal22@gmail.com>
2026-05-02 04:51:11 +0200

    v1.2

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DGS Stammtisch Hamburg – Service Worker
    =================================================== */
 
-const CACHE_NAME = 'dgs-hh-v1';
+const CACHE_NAME = 'dgs-hh-v2';
 
 const PRECACHE = [
   './',
