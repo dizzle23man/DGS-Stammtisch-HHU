d76c9170307e58d9868514ba6a96b16d81d05124
dizzle23man <richard.pal22@gmail.com>
2026-05-03 00:49:44 +0200

    v1.2.9
    refresh telegram news, fix to darkmode or lightmode background color, adding automatic rendering

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DGS Stammtisch Hamburg – Service Worker
    =================================================== */
 
-const CACHE_NAME = 'dgs-hh-v9';
+const CACHE_NAME = 'dgs-hh-v10';
 
 const PRECACHE = [
   './',
