b610fc536243af394d7cad0cd5dc0b8e61bbb2de
dizzle23man <richard.pal22@gmail.com>
2026-05-02 06:10:45 +0200

    v1.2.4
    Added Moia Link and Routing

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DGS Stammtisch Hamburg – Service Worker
    =================================================== */
 
-const CACHE_NAME = 'dgs-hh-v4';
+const CACHE_NAME = 'dgs-hh-v5';
 
 const PRECACHE = [
   './',
