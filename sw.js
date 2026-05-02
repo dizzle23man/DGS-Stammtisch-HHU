5a0ad92f08d74631b57132f46c9d6d993aa6d711
dizzle23man <richard.pal22@gmail.com>
2026-05-03 00:31:25 +0200

    v1.2.7
    added instagram grid, removed insta_placeholder, some replacecements

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    DGS Stammtisch Hamburg – Service Worker
    =================================================== */
 
-const CACHE_NAME = 'dgs-hh-v6';
+const CACHE_NAME = 'dgs-hh-v8';
 
 const PRECACHE = [
   './',
